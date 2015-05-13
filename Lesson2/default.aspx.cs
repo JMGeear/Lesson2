8ae9a73dc58f347f636ef831e72b12823ffcc6d8
Jeff Geear <jmgeear@gmail.com>
2015-05-13 13:22:26 -0400

    Commit Lesson2

---
+++ Lesson2/default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lesson2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblMessage.Text = "";
+                btnCopyText.Enabled = true;
+            }
+        }
+
+        protected void btnCopyText_Click(object sender, EventArgs e)
+        {
+            //copy the text boxto the label
+            lblMessage.Text = txtMessage.Text;
+            btnCopyText.Enabled = false;
+        }
+
+        protected void buttonShowSelections_Click(object sender, EventArgs e)
+        {
+            lblToppings.Text = "";
+            //loop through the list of checkboxes
+            foreach (ListItem topping in cblToppings.Items)
+            {
+                if (topping.Selected)
+                {
+                    lblToppings.Text += topping.Text + " ";
+                }
+            }
+            //display the selected size
+            lblSize.Text = ddlSize.SelectedItem.Text;
+        }
+
+    }
+}
